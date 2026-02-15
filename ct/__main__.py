8c09c0f21d80b24d46d91a5a6182100853be1ffe
xlea99 <alexlsomheil@gmail.com>
2026-02-14 21:54:33 -0600

    much better entrypoint

--- ct/__main__.py
+++ ct/__main__.py
@@ -1,8 +1,19 @@
-"""Entry point for `python -m ct`."""
-
+import sys
+from ct.common.logger import log
 from ct.common.setup import assert_running_from_install_root, PATHS
 from ct.app import main
 
+# Entry point for `python -m ct`
+def run() -> None:
+    try:
+        assert_running_from_install_root(PATHS.root / "clienttimer2.exe")
+        main()
+    except SystemExit:
+        raise
+    except Exception:
+        # Full stack trace, always
+        log.exception("Uncaught exception in entrypoint, exiting")
+        sys.exit(1)
 
-assert_running_from_install_root(PATHS.root / "clienttimer2.exe")
-main()
+if __name__ == "__main__":
+    run()
